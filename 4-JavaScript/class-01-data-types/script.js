a02cb80be9a06058082b4dd61dbc7d7416716bd7
Pedro Lucas <pedrolucas13083110@gmail.com>
2023-09-14 20:32:48 -0300

    add primitive class types

---
+++ 4-JavaScript/class-01-data-types/script.js
@@ -0,0 +1,28 @@
+let student = "Pedro";
+let age = 23;
+let weight = 78;
+
+console.log(typeof student);
+console.log(typeof 23);
+console.log(age);
+console.log(typeof weight);
+
+let number1 = 2;
+let number2 = 5;
+
+console.log (number1 + number2 );
+
+let isStudentInCvt = true;
+console.log(isStudentInCvt);
+console.log(typeof 
+isStudentInCvt);
+
+console.log(10 == 10);
+console.log(10 == 20);
+console.log(10 > 20);
+console.log(10 < 20);
+console.log(10 != 20);
+
+let yourName = prompt("Digite seu nome: ");
+
+console.log(yourName);
