c1d165c33827b8c556c8a8c903cb7fb6aa5f291b
Pedro Lucas <pedrolucas13083110@gmail.com>
2023-12-13 20:54:05 -0300

    add class 05 css in js

---
+++ class-05-css-in-js/src/components/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+ background: ${({ theme }) => theme.colors.backgroundButton};
+ background: orange;
+ height: 56px;
+ width: 210px;
+ font-weight: bold;
+`;
---
+++ class-05-css-in-js/src/pages/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../components/Button"
+import { AppContainer } from "./styles";
+
+export function App() {
+  return (
+    <AppContainer>
+    <h1>Class Css in JS</h1>
+    <Button/>
+    </AppContainer>
+  );
+}
